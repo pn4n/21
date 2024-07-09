ffc1c41df8f44fa7a8fba4fd10c9a2e39907ce03
kek <staciaar@student.21-school.ru>
2024-07-09 14:59:03 +0300

    multiple hui

--- r.sh
+++ r.sh
@@ -9,6 +9,78 @@ style=true
 #gcc flags
 stupid_glags="-fsanitize=address -fsanitize=leak -fsanitize=undefined -fsanitize=unreachable"
 
+handle_single_file() {
+    # check if <filename> or test.c exists
+    if [ -f $target ]; then
+        echo 'CLAMG??'
+
+        if $style; then clang-format -i $target; fi
+
+        echo 'CLAMG FOMAT SAID: '$(clang-format -n $target)
+
+        cppcheck --enable=all --suppress=missingIncludeSystem $target
+
+        # echo $(gcc -x c -std=c11 -Wall -Werror -Wextra $target $link);
+        # Check grinder, if so DONT put stupid flags
+        if $grinder; then
+            gcc -x c -std=c11 -Wall -Werror -Wextra $target $link;
+        else
+            gcc -x c -std=c11 -Wall -Werror -Wextra $target $link $stupid_glags;
+        fi
+
+        if [ $? -ne 0 ]; then
+            echo "EBANIY ROT ETOGO KAZINO"
+            exit 1
+        fi
+        echo 'COMPILEMD!!!'
+
+        if $grinder; then $(valgrind --tool=memcheck --leak-check=yes --track-origins=yes -s ./a.out);
+        else
+            printf "*-*-*-*-*-*-*-*-*-*-*result*-*-*-*-*-*-*-*-*-*-*\n\n"
+            ./a.out
+        fi
+    else
+        echo "hey bitch your $target sucks cos such file doesnt exist"
+        exit 1
+    fi
+}
+
+handle_multiple_files() {
+    local files=("$@")
+    echo "Processing multiple files: ${files[*]}"
+    for file in "${files[@]}"; do
+        echo "Processing file: $file"
+
+        if [ ! -f "$file" ]; then
+            echo "hey bitch your $file sucks cos such file doesnt exist"
+            exit 1
+        fi
+
+        cppcheck --enable=all --suppress=missingIncludeSystem $files
+
+        # gcc -x c -std=c11 -Wall -Werror -Wextra $files $link;
+        if $grinder; then
+            gcc -x c -std=c11 -Wall -Werror -Wextra $target $link;
+        else
+            gcc -x c -std=c11 -Wall -Werror -Wextra $target $link $stupid_glags;
+        fi
+
+        if [ $? -ne 0 ]; then
+            echo "EBANIY ROT ETOGO KAZINO"
+            exit 1
+        fi
+        echo 'COMPILEMD!!!'
+
+        if $grinder; then $(valgrind --tool=memcheck --leak-check=yes --track-origins=yes -s ./a.out);
+        else
+            printf "*-*-*-*-*-*-*-*-*-*-* $file *-*-*-*-*-*-*-*-*-*-*\n\n"
+            ./a.out
+        fi
+        # Add your processing commands here
+#         cat "$file"
+    done
+}
+
 # Function to print usage
 usage() {
     echo "\nUsage:  $0  \t\t| run test.c (wth gcc flags)\n"
@@ -45,44 +117,54 @@ if [ -f ../materials/linters/.clang-format ]; then
     mv ../materials/linters/.clang-format .
 fi
 
-# tun test.c if no <filename> in args
-if [ $# -ne 1 ]
-  then
-    echo "checking test.c ..."
-    target='test.c'
-  else
-    target=$1
+# run test.c if or <filename>
+if [ $# -lt 2 ]; then
+    if [ $# -eq 0 ] #run test.c
+    then
+        echo "checking test.c ..."
+        target='test.c'
+    else #run <filename.c>
+        target="$1"
+    fi
+    handle_single_file
+else
+    target=("$@")
+    # echo "array mode for:" $target
+    handle_multiple_files "${target[@]}"
 fi
 
 # check if <filename> or test.c exists
-if [ -f $target ]; then
-    echo 'CLAMG??'
+# if [ -f $target ]; then
+#     echo 'CLAMG??'
+#
+#     if $style; then clang-format -i $target; fi
+#
+#     echo 'CLAMG FOMAT SAID: '$(clang-format -n $target)
+#
+#     cppcheck --enable=all --suppress=missingIncludeSystem $target
+#
+#     echo gcc -x c -std=c11 -Wall -Werror -Wextra $target $link;
+#     # Check grinder, if so DONT put stupid flags
+#     if $grinder; then
+#         gcc -x c -std=c11 -Wall -Werror -Wextra $target $link;
+#     else
+#         gcc -x c -std=c11 -Wall -Werror -Wextra $target $link $stupid_glags;
+#     fi
+#
+#     if [ $? -ne 0 ]; then
+#         echo "EBANIY ROT ETOGO KAZINO"
+#         exit 1
+#     fi
+#     echo 'COMPILEMD!!!'
+#
+#     if $grinder; then $(valgrind --tool=memcheck --leak-check=yes --track-origins=yes -s ./a.out);
+#     else
+#         printf "*-*-*-*-*-*-*-*-*-*-*result*-*-*-*-*-*-*-*-*-*-*\n\n"
+#         ./a.out
+#     fi
+# else
+#     echo "hey bitch your $target sucks cos such file doesnt exist"
+#     exit 1
+# fi
 
-    if $style; then clang-format -i $target; fi
-
-    echo 'CLAMG FOMAT SAID: '$(clang-format -n $target)
-
-    cppcheck --enable=all --suppress=missingIncludeSystem $target
-
-    # Check grinder, if so DONT put stupid flags
-    if $grinder; then
-        gcc -x c -std=c11 -Wall -Werror -Wextra $target $link;
-    else 
-        gcc -x c -std=c11 -Wall -Werror -Wextra $target $link $stupid_glags;
-    fi
-
-    if [ $? -ne 0 ]; then
-        echo "EBANIY ROT ETOGO KAZINO"
-        exit 1
-    fi
-    echo 'COMPILEMD!!!'
-
-    if $grinder; then $(valgrind --tool=memcheck --leak-check=yes --track-origins=yes -s ./a.out); 
-    else
-        printf "*-*-*-*-*-*-*-*-*-*-*result*-*-*-*-*-*-*-*-*-*-*\n\n"
-        ./a.out
-    fi
-else
-    echo "hey bitch your $target sucks cos such file doesnt exist"
-    exit 1
-fi
+# find . -maxdepth 3 -type f -exec clang-format -i {} \;
